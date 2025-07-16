e82469e3344712ed0a378c6788cdc2c463d41806
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-07-16 11:17:22 +0530

    initial comment

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <h1>Welcome to My Portfoli</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
